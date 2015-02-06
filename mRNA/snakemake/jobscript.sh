1920f2db9f72d9271b08abfa0328cad2cd8e79f2
Kenneth Daily <kenneth.daily@sagebase.org>
2015-02-06 13:22:41 -0800

    initial commit

---
+++ mRNA/snakemake/jobscript.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#rule: {job}
+#input: {job.input}
+#output: {job.output}
+cd $SGE_O_WORKDIR
+workon snakemake
+snakemake --snakefile {self.workflow.snakefile} \
+--force -j{self.cores} \
+--directory {workdir} --nocolor --notemp --quiet --nolock {job.output} \
+> /dev/null && touch "{jobfinished}" || touch "{jobfailed}"
+exit 0
