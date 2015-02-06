b8c66d5fe39a16b726309bcbfd00d18190a3fdfb
Kenneth Daily <kenneth.daily@sagebase.org>
2015-02-06 15:35:30 -0800

    updated queries to get public bams

--- mRNA/code/R/makeSampleTable.R
+++ mRNA/code/R/makeSampleTable.R
@@ -6,22 +6,16 @@ library(reshape2)
 synapseLogin()
 
 # Get public mapped bams
-qMapped <- "select UID,id,bamType,benefactorId from file where parentId=='syn1773111' and fileType=='bam'"
-resMapped <- tbl_df(synQuery(qMapped))
-colnames(resMapped) <- gsub(".*\\.", "", colnames(resMapped))
-
-# Get public unmapped bams
-qUnmapped <- "select UID,id,bamType,benefactorId from file where parentId=='syn2246519'"
-resUnmapped <- tbl_df(synQuery(qUnmapped))
-colnames(resUnmapped) <- gsub(".*\\.", "", colnames(resUnmapped))
-resUnmapped$bamType <- "unmapped"
+qPublic <- "select UID,id,bamType,benefactorId from file where benefactorId=='syn1773109' and fileType=='bam'"
+resPublic <- tbl_df(synQuery(qPublic))
+colnames(resPublic) <- gsub(".*\\.", "", colnames(resPublic))
 
 # Get private mapped and unmapped bams
 qPrivate <- "select UID,id,bamType,benefactorId from file where parentId=='syn2261941' and fileType=='bam'"
 resPrivate <- tbl_df(synQuery(qPrivate))
 colnames(resPrivate) <- gsub(".*\\.", "", colnames(resPrivate))
 
-res <- rbind(resMapped, resUnmapped, resPrivate)
+res <- rbind(resPublic, resPrivate)
 
 resByBamType <- dcast(res, UID ~ bamType, value.var="id")
 bad <- apply(resByBamType[, c("mapped", "unmapped")], 1, function(x) any(is.na(x)))
